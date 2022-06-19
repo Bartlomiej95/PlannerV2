003342309f80c19182051abca71f127356ec2394
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-06-19 19:43:42 +0200

    Added TypOrm config - connection with mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import {TaskItem} from "./task/task.entity";
+import {User} from "./user/user.entity";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    "type": 'mysql',
+    "host": process.env.DB_HOST,
+    "port": parseInt(process.env.DB_PORT),
+    "username": process.env.DB_USERNAME,
+    "database": process.env.DB_DATABASE,
+    "password": process.env.DB_PASSWORD,
+    "entities": [TaskItem, User],
+    "bigNumberStrings": false,
+    "logging": true,
+    "synchronize": true,
+  }), UserModule, TaskModule ],
   controllers: [AppController],
   providers: [AppService],
 })
