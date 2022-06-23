336324de214690b95aa6abeaf3a9f6c6eb02dc32
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-06-23 21:43:53 +0200

    Added to ProjectController method: Get, Post, Put, Delete and handling functions in ProjectService

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Inject, Param, Post, Put} from '@nestjs/common';
+import {ProjectService} from "./project.service";
+import {ProjectItem} from "./project.entity";
 
 @Controller('project')
-export class ProjectController {}
+export class ProjectController {
+    constructor(
+        @Inject(ProjectService) private projectService: ProjectService
+    ) {
+    }
+
+    @Get('/:projectId')
+    getOne(
+        @Param('projectId') id: string
+    ): Promise<ProjectItem>{
+        return this.projectService.getOneProject(id)
+    }
+
+    @Post('/')
+    addNewProject(
+        @Body() newProject: ProjectItem
+    ): Promise<string>{
+        return this.projectService.addNewProject(newProject)
+    }
+
+    @Put('/')
+    updateProject(
+        @Body() updatedProjectData: ProjectItem
+    ): Promise<string> {
+        return this.projectService.updateProject(updatedProjectData)
+    }
+
+    @Delete('/:projectId')
+    removeProject(
+        @Param('projectId') id: string
+    ): Promise<string>{
+        return this.projectService.removeProject(id)
+    }
+}
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
+import {ProjectItem} from "./project.entity";
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+
+    async getOneProject(id: string): Promise<ProjectItem> {
+        try {
+            return await ProjectItem.findOneOrFail({ where: { id }})
+        } catch (e){
+            throw new Error('Nie znaleziono zadania o podanym id')
+        }
+    }
+
+    async addNewProject(newItem: ProjectItem): Promise<string> {
+        const newProject = new ProjectItem();
+        try {
+            newProject.title = newItem.title;
+            newProject.customer = newItem.customer;
+            newProject.deadline = newItem.deadline;
+            newProject.description = newItem.description;
+            newProject.duration = newItem.duration;
+            newProject.projectValue = newItem.projectValue;
+            newProject.scopeOfWork = newItem.scopeOfWork;
+
+            await newProject.save();
+
+            return newProject.id;
+
+        } catch (e){
+            throw new Error('Dodawanie nowego projektu nie powiodło się')
+        }
+    }
+
+    async updateProject(updatedProjectData: ProjectItem): Promise<string> {
+        try{
+            const updatedProjected = await ProjectItem.findOneOrFail({where:{id: updatedProjectData.id}});
+            updatedProjected.title = updatedProjectData.title;
+            updatedProjected.customer = updatedProjectData.customer;
+            updatedProjected.deadline = updatedProjectData.deadline;
+            updatedProjected.description = updatedProjectData.description;
+            updatedProjected.duration = updatedProjectData.duration;
+            updatedProjected.projectValue = updatedProjectData.projectValue;
+            updatedProjected.scopeOfWork = updatedProjectData.scopeOfWork;
+
+            await updatedProjected.save()
+
+            return updatedProjected.title;
+
+        }catch (e) {
+            throw new Error('Aktualizacja projektu nie powiodła się.');
+        }
+    }
+
+    async removeProject(id: string): Promise<string> {
+        try {
+            const removingProject = await ProjectItem.findOneOrFail({where: {id}})
+
+            await removingProject.remove();
+
+            return removingProject.title;
+
+        } catch (e){
+            throw new Error(`Usuwanie projektu nie powiodło się`)
+        }
+        throw new Error('Method not implemented.');
+    }
+}
