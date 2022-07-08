f25423798b39f3efc0d21364b8c2b7d047589383
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-07-08 23:03:21 +0200

    Added Department Item to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { UserItem} from "./user/user.entity";
 import { ProjectModule } from './project/project.module';
 import {ProjectItem} from "./project/project.entity";
 import { AuthModule } from './auth/auth.module';
+import { DepartmentModule } from './department/department.module';
+import {DepartmentItem} from "./department/department.entity";
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -19,11 +21,11 @@ import { AuthModule } from './auth/auth.module';
     "username": process.env.DB_USERNAME,
     "database": process.env.DB_DATABASE,
     "password": process.env.DB_PASSWORD,
-    "entities": [TaskItem, UserItem, ProjectItem],
+    "entities": [TaskItem, UserItem, ProjectItem, DepartmentItem],
     "bigNumberStrings": false,
     "logging": true,
     "synchronize": true,
-  }), UserModule, TaskModule, ProjectModule, AuthModule ],
+  }), UserModule, TaskModule, ProjectModule, AuthModule, DepartmentModule ],
   controllers: [AppController],
   providers: [AppService],
 })
